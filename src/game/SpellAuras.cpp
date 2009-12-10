42fa27871b0a5605f2e2463a4764a71e8d2be701
Insider <insider42rus@yandex.ru>
2009-12-10 17:27:47 +0700

    Reverted [patch 60]

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6753,23 +6753,6 @@ void Aura::HandleSchoolAbsorb(bool apply, bool Real)
             m_modifier.m_amount += (int32)DoneActualBenefit;
         }
     }
-    else
-    {
-        // Shadow Embrace (remove triggered spell)
-        if (m_spellProto->SpellFamilyName == SPELLFAMILY_WARLOCK && m_spellProto->SpellFamilyFlags & UI64LIT(0x0000000080000000))
-        {
-            uint32 remove_spell = 0;
-            switch(m_spellProto->Id)
-            {
-                case 32386: remove_spell = 60448; break;
-                case 32388: remove_spell = 60465; break;
-                case 32389: remove_spell = 60466; break;
-                case 32390: remove_spell = 60467; break;
-                case 32391: remove_spell = 60468; break;
-             }
-             m_target->RemoveAurasDueToSpell(remove_spell);
-        }
-    }
 }
 
 void Aura::PeriodicTick()
