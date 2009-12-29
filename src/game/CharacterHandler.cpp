3a1f8637f7025fe299c7ee7f90706350f7242aad
kero99 <motrilwireless@gmail.com>
2009-12-29 17:53:52 +0100

    [K21] Fix 2 memleaks

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -353,6 +353,7 @@ void WorldSession::HandleCharCreateOpcode( WorldPacket & recv_data )
                     {
                         data << (uint8)CHAR_CREATE_UNIQUE_CLASS_LIMIT;
                         SendPacket( &data );
+						delete result2;
                         return;
                     }
                 }
@@ -407,6 +408,7 @@ void WorldSession::HandleCharCreateOpcode( WorldPacket & recv_data )
                         {
                             data << (uint8)CHAR_CREATE_UNIQUE_CLASS_LIMIT;
                             SendPacket( &data );
+							delete result2;
                             return;
                         }
                     }
