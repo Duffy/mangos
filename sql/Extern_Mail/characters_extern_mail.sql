c07e2fa1c9fbaf11a0074e5235624836af61da65
kero99 <motrilwireless@gmail.com>
2010-01-05 17:40:40 +0100

    [K12] Update External Mail

--- sql/Extern_Mail/characters_extern_mail.sql
+++ sql/Extern_Mail/characters_extern_mail.sql
@@ -1,10 +1,11 @@
+DROP TABLE IF EXISTS `mail_external`;
 CREATE TABLE `mail_external` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
-  `receiver` bigint(20) unsigned NOT NULL,
-  `subject` varchar(200) default 'Support Message',
-  `message` varchar(500) default 'Support Message',
-  `money` bigint(20) unsigned NOT NULL default '0',
-  `item` bigint(20) unsigned NOT NULL default '0',
-  `item_count` bigint(20) unsigned NOT NULL default '0',
+  `id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `receiver` bigint(20) UNSIGNED NOT NULL,
+  `subject` varchar(200) DEFAULT 'Support Message',
+  `message` varchar(500) DEFAULT 'Support Message',
+  `money` int(20) UNSIGNED NOT NULL DEFAULT '0',
+  `item` int(20) UNSIGNED NOT NULL DEFAULT '0',
+  `item_count` int(20) UNSIGNED NOT NULL DEFAULT '1',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
