5a489ab32074439e9fed469b9bdb42af3e234da8
kero99 <motrilwireless@gmail.com>
2010-02-06 17:03:01 +0100

    [K48] Crashfix, check for getNGrid null values

--- src/game/Map.h
+++ src/game/Map.h
@@ -497,7 +497,7 @@ class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>, public MaNGOS::Obj
             return i_grids[x][y];
         }
 
-        bool isGridObjectDataLoaded(uint32 x, uint32 y) const { return getNGrid(x,y)->isGridObjectDataLoaded(); }
+        bool isGridObjectDataLoaded(uint32 x, uint32 y) const { if (getNGrid(x,y)) (return getNGrid(x,y)->isGridObjectDataLoaded(); }
         void setGridObjectDataLoaded(bool pLoaded, uint32 x, uint32 y) { getNGrid(x,y)->setGridObjectDataLoaded(pLoaded); }
 
         void setNGrid(NGridType* grid, uint32 x, uint32 y);
@@ -506,7 +506,7 @@ class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>, public MaNGOS::Obj
         void SendObjectUpdates();
         std::set<Object *> i_objectsToClientUpdate;
     protected:
-        void SetUnloadReferenceLock(const GridPair &p, bool on) { getNGrid(p.x_coord, p.y_coord)->setUnloadReferenceLock(on); }
+        void SetUnloadReferenceLock(const GridPair &p, bool on) { if (getNGrid(p.x_coord, p.y_coord)) getNGrid(p.x_coord, p.y_coord)->setUnloadReferenceLock(on); }
 
         typedef MaNGOS::ObjectLevelLockable<Map, ACE_Thread_Mutex>::Lock Guard;
 
